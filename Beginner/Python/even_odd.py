3edd63f211c4b223237f474c1bbc67e7f9eb405f
Prashant <verma.prashant225001@gmail.com>
2021-10-02 13:57:53 +0530

    Changing readme and adding random number file

--- Beginner/Python/even_odd.py
+++ Beginner/Python/even_odd.py
@@ -5,3 +5,5 @@ if (num % 2) == 0:
    print("{0} is Even".format(num))  #output is even for even number 
 else:
    print("{0} is Odd".format(num))   #output is odd for odd number 
+
+   
---
+++ Beginner/Python/random.py
@@ -0,0 +1,1 @@
+#this file provides a random number 
