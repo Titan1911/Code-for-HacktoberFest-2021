6763650aa1a53b4c3ea44aaa254e8a201cf29b33
Bharath <bharath0996@gmail.com>
2021-10-02 18:53:36 +0530

    Documented the class EvenOdd

--- Beginner/Java/EvenOdd.java
+++ Beginner/Java/EvenOdd.java
@@ -12,6 +12,10 @@ public class EvenOdd {
             System.out.println("false");
     }
 
+    /*
+     * Takes a number as input and Returns true if the given number is Even or false
+     * if not
+     */
     private static boolean isEven(int number) {
         return number % 2 == 0;
     }
