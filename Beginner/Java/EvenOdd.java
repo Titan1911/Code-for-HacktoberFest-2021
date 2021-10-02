02fafb97f8754cf6504d0a9685478669402fc256
Bharath <bharath0996@gmail.com>
2021-10-02 18:44:34 +0530

    Added class EvenOdd

---
+++ Beginner/Java/EvenOdd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        if (isEven(number))
+            System.out.println("true");
+        else
+            System.out.println("false");
+    }
+
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
