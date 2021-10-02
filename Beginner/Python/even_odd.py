2cbe141a318089462340529a02e7e952284e36b8
Prashant <verma.prashant225001@gmail.com>
2021-10-02 13:46:28 +0530

    adding even odd file

---
+++ Beginner/Python/even_odd.py
@@ -0,0 +1,7 @@
+#copyright prashant-2906
+
+num = int(input("Enter a number: ")) # enter any number 
+if (num % 2) == 0:
+   print("{0} is Even".format(num))  #output is even for even number 
+else:
+   print("{0} is Odd".format(num))   #output is odd for odd number 
