f8e5128f45c1b285d67ac251aec78bd99f7cbbdc
Bharath <bharath0996@gmail.com>
2021-10-02 19:13:56 +0530

    Formatted the files and remove unwanted code

--- Beginner/Java/CheckPrime.java
+++ Beginner/Java/CheckPrime.java
@@ -1,22 +1,11 @@
 package Beginner.Java;
 
-import java.util.Scanner;
-
 public class CheckPrime {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
-        if (isPrime(number))
-            System.out.println("true");
-        else
-            System.out.println("false");
-    }
-
     /*
      * Takes a number as input and Returns true if the given number is prime or
      * false if not
      */
-    private static boolean isPrime(int number) {
+    public boolean isPrime(int number) {
         for (int i = 2; i <= Math.sqrt(number); i++)
             if (number % i == 0)
                 return false;
--- Beginner/Java/EvenOdd.java
+++ Beginner/Java/EvenOdd.java
@@ -1,22 +1,11 @@
 package Beginner.Java;
 
-import java.util.Scanner;
-
 public class EvenOdd {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
-        if (isEven(number))
-            System.out.println("true");
-        else
-            System.out.println("false");
-    }
-
     /*
      * Takes a number as input and Returns true if the given number is Even or false
      * if not
      */
-    private static boolean isEven(int number) {
+    public boolean isEven(int number) {
         return number % 2 == 0;
     }
 }
