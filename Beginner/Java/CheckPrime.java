00d87a9ea3570a8d8b23c0ef19c0deeccd375a70
Bharath <bharath0996@gmail.com>
2021-10-02 18:52:31 +0530

    Add class CheckPrime

---
+++ Beginner/Java/CheckPrime.java
@@ -0,0 +1,25 @@
+package Beginner.Java;
+
+import java.util.Scanner;
+
+public class CheckPrime {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        if (isPrime(number))
+            System.out.println("true");
+        else
+            System.out.println("false");
+    }
+
+    /*
+     * Take a number as input and Returns true if the given number is prime or false
+     * if not
+     */
+    private static boolean isPrime(int number) {
+        for (int i = 2; i <= Math.sqrt(number); i++)
+            if (number % i == 0)
+                return false;
+        return true;
+    }
+}
