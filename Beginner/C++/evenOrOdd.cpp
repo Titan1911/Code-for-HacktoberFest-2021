758f05f0b8408ae4997784cd8e9575fa6ad4b950
Mayur S Chittaragi <62736724+MayurSChittaragi@users.noreply.github.com>
2021-10-02 18:59:46 +0530

    Add evenOrOdd.cpp

---
+++ Beginner/C++/evenOrOdd.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+        if(n%2 == 0){
+            cout<<"Even"<<endl;
+        }
+        else{
+            cout<<"Odd"<<endl;
+        }
+    }
+    return 0;
+}
